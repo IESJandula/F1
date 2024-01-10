fc3a9bc37c049e6dab2e8b91405f3074b2926e98
franklin3499@gmail.com <145580354+francis65000@users.noreply.github.com>
2024-01-10 16:22:01 +0100

    Arbol de archivos y carpetas

---
+++ Proyecto-F1/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>F1 Ranking</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+        <?php include("includes/menu.php")?>
+        <h1>Página de inicio</h1>
+    
+        
+    </body>
+</html>
