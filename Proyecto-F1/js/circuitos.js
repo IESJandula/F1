d04b9c12c5e7762e0fc3a348bcbaa39a89c8768b
franklin3499@gmail.com <145580354+francis65000@users.noreply.github.com>
2024-01-11 16:44:18 +0100

    Circuitos completo.js

--- Proyecto-F1/js/circuitos.js
+++ Proyecto-F1/js/circuitos.js
@@ -14,47 +14,49 @@ async function cargarDatos() {
 }
 
 // Función que se encarga de mostrar los datos en el HTML
+// Función que se encarga de mostrar los datos en tarjetas HTML
 function mostrarDatos(circuitos) {
-    // Obtener la referencia del elemento body
+    // Obtener la referencia del elemento con el id "caja"
     var divCircuitos = document.getElementById("caja");
-    // Crear un elemento <table> y un elemento <tbody>
-    var tabla = document.createElement("table");
-    var tblBody = document.createElement("tbody");
-    // Crear las celdas
+
+    // Crear las tarjetas
     for (var i = 0; i < circuitos.length; i++) {
-        // Crear las hileras de la tabla
-        var hilera = document.createElement("tr");
-        // Crear un elemento <td> y un nodo de texto, haz que el nodo de
-        // texto sea el contenido de <td>, ubica el elemento <td> al final
+        // Crear un elemento <a> para la tarjeta
+        var tarjeta = document.createElement("a");
+        tarjeta.href = "#";
+        tarjeta.className = "flex flex-col items-center bg-black border border-gray-200 rounded-lg shadow md:flex-row md:max-w-2xl hover:bg-gray-700 mx-2";
 
-        //ID DEL CIRCUITO
-        var celda = document.createElement("td");
-        var textoCelda = document.createTextNode(circuitos[i].circuit.id);
-        celda.appendChild(textoCelda);
-        hilera.appendChild(celda);
-
-        //NOMBRE DEL CIRCUITO
-        var celda = document.createElement("td");
-        var textoCelda = document.createTextNode(circuitos[i].circuit.name);
-        celda.appendChild(textoCelda);
-        hilera.appendChild(celda);
-
-        //IMAGEN DEL CIRCUITO
-        var celda = document.createElement("td");
+        // Crear un elemento <img> para la imagen del circuito
         var imagen = document.createElement('img');
+        imagen.className = "object-cover w-56 h-56 md:w-56 md:h-56 md:rounded-none md:rounded-l-lg";
         imagen.src = circuitos[i].circuit.image;
-        celda.appendChild(imagen);
-        hilera.appendChild(celda);
+        imagen.alt = "image_circuit";
+        tarjeta.appendChild(imagen);
 
-        // Agregar la hilera al final de la tabla (al final del elemento tblBody)
-        tblBody.appendChild(hilera);
+        // Crear un elemento <div> para el contenido de la tarjeta
+        var contenido = document.createElement("div");
+        contenido.className = "flex flex-col justify-between p-4 leading-normal w-56";
+
+        // Crear un elemento <h5> para el nombre del circuito
+        var nombre = document.createElement("h5");
+        nombre.className = "mb-2 text-2xl font-bold tracking-tight text-white";
+        var textoNombre = document.createTextNode(circuitos[i].circuit.name);
+        nombre.appendChild(textoNombre);
+        contenido.appendChild(nombre);
+
+        // Crear un elemento <p> para la información del circuito (país y ciudad)
+        var info = document.createElement("p");
+        info.className = "mb-3 font-normal text-gray-700";
+        var textoInfo = document.createTextNode("País: " + circuitos[i].competition.location.country + "<br />Ciudad: " + circuitos[i].competition.location.city);
+        info.innerHTML = textoInfo.textContent;
+        contenido.appendChild(info);
+
+        // Agregar el contenido al elemento de la tarjeta
+        tarjeta.appendChild(contenido);
+
+        // Agregar la tarjeta al contenedor principal
+        divCircuitos.appendChild(tarjeta);
     }
-    // Posicionar el <tbody> debajo del elemento <table>
-    tabla.appendChild(tblBody);
-    // Añadir <table> al elemento con el id "circuitos"
-    divCircuitos.appendChild(tabla);
-    // Modificar el atributo "border" de la tabla y fijarlo a "2";
-    tabla.setAttribute("border", "2");
 }
 
 // Llamar a la función para cargar datos al cargar la página
